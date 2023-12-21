e462a7b892ac48d3ff3dab058837871434cb513f
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-12-21 09:05:14 -0500

    Añadidos getters y setters, dependencias corregidas

--- src/main/java/com/example/Venta.java
+++ src/main/java/com/example/Venta.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example;
 
 import java.io.Serializable;
 import java.util.Date;
-
-/**
- *
- * @author CRISTIAN
- */
 
 public class Venta implements Serializable {
 
@@ -21,10 +12,65 @@ public class Venta implements Serializable {
     private String marca;
     private Date fechaVenta;
 
-    // getters y setters
-
-    // constructor sin argumentos
+    // Constructor sin argumentos
     public Venta() {
         this.fechaVenta = new Date(); // La fecha de venta se establece al momento de la creación de la venta
     }
+
+    // Getter para el código
+    public String getCodigo() {
+        return codigo; 
+    }
+
+    // Setter para el código
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    // Getter para el ítem
+    public String getItem() {
+        return item;
+    }
+
+    // Setter para el ítem
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    // Getter para la descripción
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    // Setter para la descripción
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    // Getter para el precio
+    public double getPrecio() {
+        return precio;
+    }
+
+    // Setter para el precio
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    // Getter para la marca
+    public String getMarca() { 
+        return marca;
+    }
+
+    // Setter para la marca
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    // Getter para la fecha de venta
+    public Date getFechaVenta() {
+        return fechaVenta;
+    }
+
+    // Setter para la fecha de venta (no se proporciona un setter para la fecha de venta ya que debería establecerse solo al crear la venta)
 }
